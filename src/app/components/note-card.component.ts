3958f42c83eceb0d37d922a3192ea7e94aa31ee6
Poorani K <poorani@petras.in>
2025-07-22 11:11:59 +0530

    Style fixes

--- src/app/components/note-card.component.ts
+++ src/app/components/note-card.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Note } from '../services/note.service';
 import { CommonModule } from '@angular/common';
-
+import {RouterModule} from '@angular/router'
 @Component({
   selector: 'app-note-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   templateUrl: './note-card.component.html',
   styleUrls: ['./note-card.component.scss']
 })
